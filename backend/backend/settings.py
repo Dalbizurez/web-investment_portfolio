25f0062d2bd52489ca4fd4f85ad5ed24335c87c3
JuanMaCE <jk120365@gmail.com>
2025-09-29 11:35:21 -0600

    update files to work with mysql

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'usuarios',
 ]
 
 MIDDLEWARE = [
@@ -74,8 +75,12 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'mydb',
+        'USER': 'root',
+        'PASSWORD': '120365',
+        'HOST': 'localhost',   # o la IP/host del servidor SQL
+        'PORT': '3306',
     }
 }
 
