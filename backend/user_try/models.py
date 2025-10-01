427801fe1c0bd6edc01d512dbb789c0c5d816535
JuanMaCE <jk120365@gmail.com>
2025-10-01 09:37:48 -0600

    test register without mail verificawtion

---
+++ backend/user_try/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+class User(models.Model):
+    USER_TYPES = [
+        ("standard", "Standard"),
+        ("admin", "Admin"),
+        ("vip", "VIP"),
+    ]
+
+    username = models.CharField(max_length=100, unique=True)
+    email = models.EmailField(unique=True)
+    password = models.CharField(max_length=128)  
+    type = models.CharField(max_length=50, choices=USER_TYPES, default="standard")
+
+    def __str__(self):
+        return self.username
