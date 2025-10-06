690532beb726d856a7404e0f80299f7f64c54bc2
DanielJuarez1645122 <eduardojuarez2602@gmail.com>
2025-10-05 18:01:29 -0600

    feat(secondSection): Agregue la tercera seccion la cual es responsive igual que la segunda seccion

--- frontend/frontend/src/pages/home_page.tsx
+++ frontend/frontend/src/pages/home_page.tsx
@@ -1,11 +1,13 @@
 import Navbar from "../components/navbar";
 import FirstSection from "../components/hp_first_section";
+import SecondSection from "../components/hp_second_sectionc";
 
 function HomePage() {
   return (
     <>
       <Navbar />
       <FirstSection/>
+      <SecondSection />
     </>
   );
 }
