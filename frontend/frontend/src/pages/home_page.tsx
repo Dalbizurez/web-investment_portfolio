fcebf58861929551ef75044e3577b104156d977a
DanielJuarez1645122 <eduardojuarez2602@gmail.com>
2025-10-05 19:21:22 -0600

    feat(ThirdSection): Agregue la tercera seccion responsive de la homepage

--- frontend/frontend/src/pages/home_page.tsx
+++ frontend/frontend/src/pages/home_page.tsx
@@ -1,6 +1,7 @@
 import Navbar from "../components/navbar";
 import FirstSection from "../components/hp_first_section";
 import SecondSection from "../components/hp_second_sectionc";
+import ThirdSection from "../components/hp_third_section";
 
 function HomePage() {
   return (
@@ -8,6 +9,7 @@ function HomePage() {
       <Navbar />
       <FirstSection/>
       <SecondSection />
+      <ThirdSection />
     </>
   );
 }
