4044039079748f5169dd1d3278e58f8c561baa3f
DanielJuarez1645122 <eduardojuarez2602@gmail.com>
2025-10-05 19:41:41 -0600

    feat(FourthSection): Agregue la cuarta seccion responsive de la homepage

--- frontend/frontend/src/pages/home_page.tsx
+++ frontend/frontend/src/pages/home_page.tsx
@@ -2,6 +2,7 @@ import Navbar from "../components/navbar";
 import FirstSection from "../components/hp_first_section";
 import SecondSection from "../components/hp_second_sectionc";
 import ThirdSection from "../components/hp_third_section";
+import FourthSection from "../components/hp_fourth_section";
 
 function HomePage() {
   return (
@@ -10,6 +11,7 @@ function HomePage() {
       <FirstSection/>
       <SecondSection />
       <ThirdSection />
+      <FourthSection />
     </>
   );
 }
