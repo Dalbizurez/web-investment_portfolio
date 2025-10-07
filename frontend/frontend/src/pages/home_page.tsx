7ac3e3e411ab92bcdd9a0215e47cd67a2e25e6ef
DanielJuarez1645122 <eduardojuarez2602@gmail.com>
2025-10-07 14:11:10 -0600

    feat(SixthSection): Agregue la sexta seccion del carousel junto con responsive

--- frontend/frontend/src/pages/home_page.tsx
+++ frontend/frontend/src/pages/home_page.tsx
@@ -3,6 +3,8 @@ import FirstSection from "../components/hp_first_section";
 import SecondSection from "../components/hp_second_sectionc";
 import ThirdSection from "../components/hp_third_section";
 import FourthSection from "../components/hp_fourth_section";
+import FifthSection from "../components/hp_fifth_section";
+import CarouselSection from "../components/hp_sixth_section";
 
 function HomePage() {
   return (
@@ -12,6 +14,8 @@ function HomePage() {
       <SecondSection />
       <ThirdSection />
       <FourthSection />
+      <FifthSection />
+      <CarouselSection />
     </>
   );
 }
