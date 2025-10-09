a406ebcc17a30a7e027ebbe61700a504316b2ae7
KarenLaines <144739875+KarenLaines@users.noreply.github.com>
2025-10-08 21:24:07 -0600

    feat: add the menu for the sidebar with some corrections

--- frontend/frontend/src/App.tsx
+++ frontend/frontend/src/App.tsx
@@ -1,12 +1,14 @@
 import { Routes, Route } from "react-router-dom";
 import HomePage from "./pages/home_page";
 import LoginForm from "./pages/form_login";
+import InvitePage from "./pages/invitePage";
 
 function App() {
-   return (
+  return (
     <Routes>
       <Route path="/" element={<HomePage />} />
       <Route path="/signin" element={<LoginForm />} />
+      <Route path="/invite" element={<InvitePage />} />
     </Routes>
   );
 }
