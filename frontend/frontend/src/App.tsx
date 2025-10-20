cb0ccb41eb29b29307d7efa7fe94dd949e6ffd1f
Fernando Contreras <fernandomorann999@gmail.com>
2025-10-20 10:06:07 -0600

    feat: principal App resolved

--- frontend/frontend/src/App.tsx
+++ frontend/frontend/src/App.tsx
@@ -4,6 +4,8 @@ import LoginForm from "./pages/form_login";
 import InvitePage from "./pages/invitePage";
 import SignupForm from "./pages/form_signup";
 import HomeUser from "./pages/home_user";
+import Portafolio from "./pages/portafolio";
+import TransferPage from "./pages/transfers";
 
 function App() {
   return (
@@ -13,6 +15,8 @@ function App() {
       <Route path="/invite" element={<InvitePage />} />
       <Route path="/get-started" element={<SignupForm />} />
       <Route path="/homeUser" element={<HomeUser />} />
+      <Route path="/portafolio/" element={<Portafolio/>} />
+      <Route path="/transfers" element={<TransferPage />} />
     </Routes>
   );
 }
